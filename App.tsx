95deb71c44844c3a55f17f58e9fc5bce6a78c444
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-02 00:04:56 -0300

    configuring theme

--- App.tsx
+++ App.tsx
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { THEME } from './src/styles/theme';
+
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
