9341e00eadbbb7627337e8137f8b03c59a1d067d
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-02 00:17:35 -0300

    add google fonts and create signIn screen

--- App.tsx
+++ App.tsx
@@ -1,25 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loadig';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      {fontsLoaded ?  <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
