6e8c4f6595886325090dfb831c5153890ababdac
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-02 00:19:17 -0300

    customizing statusbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme';
@@ -10,6 +10,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       {fontsLoaded ?  <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
