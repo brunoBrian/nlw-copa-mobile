d215a79ed41a54cc2558b27902dd894404942c99
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-03 19:39:15 -0300

    configuring project to use svg as component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,11 @@
 import { Center, Text } from 'native-base'
 
+import Logo from '../assets/logo.svg'
+
 export function SignIn() {
   return (
     <Center flex={1} bgColor='gray.900'>
+      <Logo />
       <Text color='white' fontSize={14} fontFamily='heading'>SignIn</Text>
     </Center>
   )
