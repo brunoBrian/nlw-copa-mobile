05956d0a6320960dd19d0fe989a28f7386405e2f
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-04 19:40:57 -0300

    create types of navigation items

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+declare module "*.svg" {
+  namespace ReactNavigation {
+    interface RootParamList {
+      new: undefined;
+      pools: undefined;
+      create: undefined;
+      details: {
+        id: string;
+      }
+    }
+  }
+}
