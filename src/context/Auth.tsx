c00d06aecffd0c4bd7b3cb9cb9e5594a270ff833
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-04 20:01:11 -0300

    get userData by api and saving in context

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 import * as GoogleAuth from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
+import { api } from '../services/api'
 
 WebBrowser.maybeCompleteAuthSession()
 
@@ -44,6 +45,25 @@ export function AuthContextProvider({ children }) {
 
   async function signInWithGoogle(access_token: string) {
     console.log('Token de autenticacao', access_token);
+
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post('/users', {
+        access_token
+      })
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data?.token}`
+
+      const userInfoResponse = await api.get('/me')
+
+      setUser(userInfoResponse.data.user)
+    } catch(err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
     
   }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { SignIn } from '../screens'
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <SignIn />
+      {/* <AppRoutes /> */}
     </NavigationContainer>
   )
 }
