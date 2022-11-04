d3044e2f77574650e5a4be7ea98dffcc474037a2
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-03 23:38:54 -0300

    implement google authentication

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,4 +1,9 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import * as GoogleAuth from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export interface UserProps {
   avatarUrl: string;
@@ -7,22 +12,52 @@ export interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
-  signIn: () => Promise<void>
+  isUserLoading: boolean;
+  signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
+
+  // promptAsync define quando vai abrir fluxo de autenticação
+  const [request, response, promptAsync] = GoogleAuth.useAuthRequest({
+    clientId: '169078025695-4f8i25ovt03geem019fefsj39vfm55rg.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({useProxy: true}), 
+    scopes: ['profile', 'email']
+  })
+
   async function signIn() {
-    console.log('Ok google');
+    try {
+      setIsUserLoading(true)
+
+      await promptAsync()
+    } catch(err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('Token de autenticacao', access_token);
+    
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success' && response?.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
+  
 
   return (
     <AuthContext.Provider value={{
-      user: {
-        name: 'Bruno',
-        avatarUrl: 'klnl'
-      },
+      isUserLoading,
+      user,
       signIn
     }}>
       {children}
